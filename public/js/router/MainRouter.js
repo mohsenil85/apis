49b0362e0ca8df04459bc35870ee4cf5c5750f62
mohsenil85 <mohsenil85@gmail.com>
2014-05-22 20:28:43 -0700

    added foot and header

--- public/js/router/MainRouter.js
+++ public/js/router/MainRouter.js
@@ -2,59 +2,67 @@ define(function(require){
 
   "use strict";
 
-    var $                 = require('jquery'),
-        _                 = require('underscore'),
-        Backbone          = require('backbone'),
-        UsersCollection   = require('collections/usersCollection'),
-        UserListView      = require('views/userListView'),
-        EditUserView      = require( 'views/editUserView'),
-        HomePageView      = require('views/homePageView'),
-        ViewUserView      = require('views/viewUserView')
+  var $                 = require('jquery'),
+  _                 = require('underscore'),
+  Backbone          = require('backbone'),
+  UsersCollection   = require('collections/usersCollection'),
+  UserListView      = require('views/userListView'),
+  EditUserView      = require( 'views/editUserView'),
+  HomePageView      = require('views/homePageView'),
+  HeaderView        = require('views/headerView'),
+  FooterView        = require('views/footerView'),
+  ViewUserView      = require('views/viewUserView')
 
-    var MainRouter = Backbone.Router.extend({
-        routes: {
-            '' : 'home',
-            'userlist' : 'userList',
-            'new' : 'editUser',
-            'edit/:id' : 'editUser',
-            'view/:id' : 'viewUser'
-        }
+  var MainRouter = Backbone.Router.extend({
+    routes: {
+      '' : 'home',
+      'userlist' : 'userList',
+      'new' : 'editUser',
+      'edit/:id' : 'editUser',
+      'view/:id' : 'viewUser'
+    }
+  });
+  var initialize = function(){
+
+    this.headerView = new HeaderView();
+    $('.header').html(this.headerView.render());
+
+    this.footerView = new FooterView();
+    $('.footer').html(this.footerView.render());
+
+    var router = new MainRouter();
+    var userList = new UserListView();
+
+    var editUserView = new EditUserView({
+      router: router
     });
-    var initialize = function(){
+    var homePageView = new HomePageView({
+      router: router
+    });
 
-        var router = new MainRouter();
-        var userList = new UserListView();
+    var viewUserView = new ViewUserView({
+      router: router
+    });
 
-        var editUserView = new EditUserView({
-            router: router
-        });
-        var homePageView = new HomePageView({
-            router: router
-        });
+    router.on('route:editUser', function(id){
+      editUserView.render({id: id});
+    });
+    router.on('route:userList', function(){
+      userList.render();
+    });
 
-        var viewUserView = new ViewUserView({
-            router: router
-        });
+    router.on('route:home', function(){
+      homePageView.render();
+    })
 
-        router.on('route:editUser', function(id){
-            editUserView.render({id: id});
-        });
-        router.on('route:userList', function(){
-            userList.render();
-        });
+    router.on('route:viewUser', function(id){
+      viewUserView.render({id: id});
+    })
 
-        router.on('route:home', function(){
-          homePageView.render();
-        })
+    Backbone.history.start();
+  };
+  return {
+    initialize: initialize
 
-        router.on('route:viewUser', function(id){
-          viewUserView.render({id: id});
-        })
-
-        Backbone.history.start();
-    };
-    return {
-        initialize: initialize
-
-    };
+  };
 });
